e2b5edeb868ea2ad110f24f6066135ece5144c4a
MohamedFalah <mohamshualh@gmail.com>
2018-11-17 21:33:57 +0300

    ASP.NET and SignUp proccess

---
+++ KFUPMSS/Services/ApiServices.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using KFUPMSS.Model;
+using Newtonsoft.Json;
+
+namespace KFUPMSS.Services
+{
+    public class ApiServices
+    {
+
+
+        public ApiServices()
+        {
+        }
+
+        internal async Task<bool> RegisterAsync(string firstName, string lastName, string id, string password)
+        {
+
+            var client = new HttpClient();
+
+            var user = new User
+            {
+
+                FirstName = firstName,
+
+                LastName = lastName,
+
+                ID = id,
+
+                Password = password
+
+            };
+
+            var json = JsonConvert.SerializeObject(user);
+
+
+            HttpContent content = new StringContent(json);
+
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            var response = await client.PostAsync("https://seniorproject003kfupm.firebaseio.com/Users", content);
+
+            return response.IsSuccessStatusCode;  
+
+        }
+    }
+}
---
+++ KFUPMSSBackend/KFUPMSSBackend/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace KFUPMSSBackend.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
